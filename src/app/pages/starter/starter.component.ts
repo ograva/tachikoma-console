14e080b9d265614a3a4d820c7f373742f09fe261
Novus Apps <ravago@gmail.com>
2025-11-22 01:30:24 +0800

    Fix missing TablerIconsModule import in StarterComponent

--- src/app/pages/starter/starter.component.ts
+++ src/app/pages/starter/starter.component.ts
@@ -1,13 +1,13 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { MaterialModule } from '../../material.module';
 import { Router } from '@angular/router';
-
-
+import { TablerIconsModule } from 'angular-tabler-icons';
 
 @Component({
   selector: 'app-starter',
   imports: [
-    MaterialModule
+    MaterialModule,
+    TablerIconsModule
   ],
   templateUrl: './starter.component.html',
   encapsulation: ViewEncapsulation.None,
