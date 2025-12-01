cc92ad2ccc58873b6ecac763b487b15486eaac40
Novus Apps <ravago@gmail.com>
2025-12-01 19:07:08 +0800

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,11 +3,12 @@ export const environment = {
   firebase: {
     // Firebase configuration - to be populated by the user
     // Get these values from Firebase Console > Project Settings > General > Your apps
-    apiKey: '',
-    authDomain: '',
-    projectId: 'tachikoma-chat',
-    storageBucket: '',
-    messagingSenderId: '',
-    appId: '',
+    apiKey: "AIzaSyDTFg-jj71u6q8iq3MrCRDzg2dSIFhHLXU",
+  authDomain: "tachikoma-chat.firebaseapp.com",
+  projectId: "tachikoma-chat",
+  storageBucket: "tachikoma-chat.firebasestorage.app",
+  messagingSenderId: "579092802298",
+  appId: "1:579092802298:web:9686cc66c809f70e073743",
+  measurementId: "G-DZB94TTCV3"
   },
 };
